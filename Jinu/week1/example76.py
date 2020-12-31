4184cc0c825da8e3cf4f4880cd9e125219ff9b29
nureong <76807044+nureong@users.noreply.github.com>
2020-12-31 21:44:29 +0900

    week1 homework
    20/12/31

---
+++ Jinu/week1/example76.py
@@ -0,0 +1,9 @@
+string = "PYTHON"
+
+print("*" * 5 + " 정방향 " + "*" * 5)
+print(string[0:2])
+print(string[4:5])
+
+print("*" * 5 + " 역방향 " + "*" * 5)
+print(string[-3:-1])
+print(string[-6:-5])
---
+++ Jinu/week1/problem79.py
@@ -0,0 +1,15 @@
+
+string = "ABCDEFG"
+
+print(string[0:1])
+print(string[1:2])
+print(string[-2:-1])
+#print(string[-1:0]) #이건 안되넹,,,
+print(string[6:7])
+print(string[3:4])
+print()
+
+print(string[0:2])
+#print(string[-2:0]) #이거도 안되넹...
+print(string[5:7])
+print(string[0:2]+string[3:6])
