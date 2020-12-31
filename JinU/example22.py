4664f012513ab52e70668cbfac7673ed3a6a9326
nureong <76807044+nureong@users.noreply.github.com>
2020-12-31 21:40:49 +0900

    Delete example22.py

--- JinU/example22.py
+++
@@ -1,7 +0,0 @@
-name = 'Kevin'
-work = 'IT company'
-num1 = 5.3
-num2 = 4.7517
-print("I'm", name, "and work in", work)
-print("I'm {} and work in {}".format(name,work))
-print("This is {:8.2}, {:8.2}".format(num1, num2))  #{: ,} ":폭. 정밀도" 폭이 8이고, 2개의 유효숫자
