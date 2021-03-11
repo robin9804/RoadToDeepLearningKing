90bf102d504e4b0cbbeef66f1dd57a5b5c3ae7d7
YoungRok <51745277+robin9804@users.noreply.github.com>
2021-03-11 16:30:26 +0900

    Add files via upload
    가중치 변경

--- Project/model/Resnet.py
+++ Project/model/Resnet.py
@@ -4,7 +4,7 @@ import torch.nn.functional as F
 
 
 class BasicBlock(nn.Module):
-    expansion = 1
+    expansion = 1  # 원래 1
 
     def __init__(self, in_planes, planes, stride=1):
         super(BasicBlock, self).__init__()
@@ -67,14 +67,14 @@ class ResNet(nn.Module):
         super(ResNet, self).__init__()
         self.in_planes = 64
 
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=3,
+        self.conv1 = nn.Conv2d(1, 64, kernel_size=3,
                                stride=1, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(64)
         self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
         self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
         self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
         self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
-        self.linear = nn.Linear(512*block.expansion, num_classes)
+        self.linear = nn.Linear(512*block.expansion * 16 * 16, num_classes)
 
     def _make_layer(self, block, planes, num_blocks, stride):
         strides = [stride] + [1]*(num_blocks-1)
@@ -85,12 +85,17 @@ class ResNet(nn.Module):
         return nn.Sequential(*layers)
 
     def forward(self, x):
-        out = F.relu(self.bn1(self.conv1(x)))
+        out = F.relu(self.bn1(self.conv1(x)))  # 512 x 512
         out = self.layer1(out)
-        out = self.layer2(out)
-        out = self.layer3(out)
-        out = self.layer4(out)
+        #print(out.shape)
+        out = self.layer2(out)  # 256 x 256
+        #print(out.shape)
+        out = self.layer3(out)  # 128 # 128
+        #print(out.shape)
+        out = self.layer4(out)  #
+        #print(out.shape)
         out = F.avg_pool2d(out, 4)
+        #print(out.shape)
         out = out.view(out.size(0), -1)
         out = self.linear(out)
         return out
